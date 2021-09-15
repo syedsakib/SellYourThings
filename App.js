c2b0b4cada427d51755ffc3d664253a5608b174c
sakib <sakibsyed01@gmail.com>
2021-09-16 04:36:32 +0600

    card

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { View } from "react-native";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#F8F4F4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for Sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
 
 {
