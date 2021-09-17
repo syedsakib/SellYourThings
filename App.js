b5e119c926483b3d07cf856581064de88d58f18a
sakib <sakibsyed01@gmail.com>
2021-09-17 15:43:40 +0600

    Icon Component

--- App.js
+++ App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 }
 
 {
